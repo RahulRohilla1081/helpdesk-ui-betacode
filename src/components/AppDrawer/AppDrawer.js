f2218c7b529b4490327355da85afdebe02370544
Rahul Rohilla <rahul.rohilla1081@gmail.com>
2024-10-13 17:09:07 +0530

    added add emp for admin

--- src/components/AppDrawer/AppDrawer.js
+++ src/components/AppDrawer/AppDrawer.js
@@ -345,7 +345,8 @@ function AppDrawerEmployee(props) {
         icon: <AssessmentIcon />,
         path: ADD_EMPLOYEES,
         SHOW:
-          props.LOGGED_IN_DATA.USER_TYPE?.includes(6) &&
+          (props.LOGGED_IN_DATA.USER_TYPE?.includes(0) ||
+            props.LOGGED_IN_DATA.USER_TYPE?.includes(6)) &&
           props.LOGGED_IN_DATA.LOGGED_IN_AS == "EMPLOYEE"
             ? true
             : false,
